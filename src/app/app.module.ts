9330cdb8d9dbf8ff9647d2c5b982e67aa2c015fb
rogerio ferreira <rogerfbatista@gmail.com>
2019-06-25 14:27:57 -0300

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule ,NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MenuComponent } from './menu/menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+      ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+     MatCheckboxModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+     ])
   ],
+  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
