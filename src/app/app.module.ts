d11bcc504429edc2aa581334a015217a13d1214c
rogerio ferreira <rogerfbatista@gmail.com>
2019-06-25 18:51:09 -0300

    componentes material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule ,NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule,
+   NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import{FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { FormscontrolComponent } from './formscontrol/formscontrol.component';
+
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatNativeDateModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatInputModule,
+  MatAutocompleteModule,
+  MatSlideToggleModule,
+  MatRadioModule,
+  MatCardModule} from '@angular/material';
+
 
 
 @NgModule({
@@ -15,19 +36,47 @@ import { MenuComponent } from './menu/menu.component';
     AppComponent,
     HomeComponent,
     MenuComponent,
-      ],
+    HeaderComponent,
+    SidenavListComponent,
+    FormscontrolComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NoopAnimationsModule,
     MatButtonModule,
-     MatCheckboxModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatNativeDateModule, MatIconModule,
+    MatListModule, MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatInputModule,
+    FormsModule,  
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatCardModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-     ])
+      { path: 'menu', component: MenuComponent },
+      { path: 'form', component: FormscontrolComponent}
+    ])
   ],
-  exports: [MatButtonModule, MatCheckboxModule],
+  exports: [MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatRadioModule,
+    MatCardModule
+     ],
   providers: [],
   bootstrap: [AppComponent]
 })
